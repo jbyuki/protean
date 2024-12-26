d4cba210ba4127674aa8a2a9ff990b5bf888ae32
Julien Burkhard <jyburkhard@hotmail.com>
2024-12-26 19:39:59 +0900

    init

--- ntpy/server.py
+++ ntpy/server.py
@@ -1,24 +1,16 @@
-;;; ntpy-server
-:: ../../ntpy/server.py
-; imports
-; variables
-; define functions
-
-;; imports
 import asyncio
 import json
 
-;; define functions
 async def start_server(host='localhost', port=8089):
 	server = await asyncio.start_server(handle_echo, host, port)
 	print(f"Server started on {port}")
 	async with server:
 		await server.serve_forever()
 
-;; define functions
 async def handle_echo(reader, writer):
 	data = await reader.read(100)
 	obj = json.loads(data)
 	print(f"Received: {obj}")
 	writer.close()
 	await writer.wait_closed()
+
